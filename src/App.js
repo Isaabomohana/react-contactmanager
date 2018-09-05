35d80c8abce915a71b66c91e4bcd38a3ae437951
Isaabomohana <mrissaam@outlook.com>
2018-09-05 15:52:11 +0200

    add components/Contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Contact from "./components/Contact";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Contact React</h1>
+        <h1>React App</h1>
+        <Contact />
       </div>
     );
   }
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Isa Abo-Mohana </h4>
+        <ul>
+          <li>Email: mrissaam@outlook.com</li>
+          <li>Mob: 555-555-5555</li>
+        </ul>
+      </div>
+    );
+  }
+}
