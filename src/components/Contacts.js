9a9f914f2fdc4bc89f08cc9fe8e25200b8cb80bb
Isaabomohana <mrissaam@outlook.com>
2018-09-06 16:25:06 +0200

    build a state & map contacts through Contact component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,45 +1,41 @@
 import React, { Component } from "react";
+import Contact from "./Contact";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "Samo",
-          email: "samo@sam.sam",
-          mob: "123-123-4444"
-        },
-        {
-          id: 2,
-          name: "Samosom",
-          email: "samosom@sam.sam",
-          mob: "123-123-5555"
-        },
-        {
-          id: 3,
-          name: "Samsomah",
-          email: "mah@sam.sam",
-          mob: "123-123-6666"
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Samo",
+        email: "samo@sam.sam",
+        mob: "123-123-4444"
+      },
+      {
+        id: 2,
+        name: "Samosom",
+        email: "samosom@sam.sam",
+        mob: "123-123-5555"
+      },
+      {
+        id: 3,
+        name: "Samsomah",
+        email: "mah@sam.sam",
+        mob: "123-123-6666"
+      }
+    ]
+  };
   render() {
     const { contacts } = this.state;
 
     return (
       <div>
         {contacts.map(contact => (
-          <div className="card card-body mb-3">
-            <h4>{contact.name} </h4>
-            <ul className="list-group">
-              <li className="list-group-item">Id: {contact.id}</li>
-              <li className="list-group-item">Email: {contact.email}</li>
-              <li className="list-group-item">Mobile: {contact.mob}</li>
-            </ul>
-          </div>
+          <Contact
+            key={contact.id}
+            name={contact.name}
+            email={contact.email}
+            mob={contact.mob}
+          />
         ))}
       </div>
     );
